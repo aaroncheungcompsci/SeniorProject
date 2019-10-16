9e427382751300343ea70b1b691bd7d52cac2e8a
Andrew Hang <andrewjameshang@csus.edu>
2019-10-16 16:14:28 -0700

    Andrew is pushing Send Email Button.

--- karmecca-crm2/src/app/app.module.ts
+++ karmecca-crm2/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { ViewAppsComponent } from './view-apps/view-apps.component';
+import { EmailButtonComponent } from './email-button/email-button.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ViewAppsComponent } from './view-apps/view-apps.component';
     AdminComponent,
     HomeComponent,
     HeaderComponent,
-    ViewAppsComponent
+    ViewAppsComponent,
+    EmailButtonComponent
   ],
   imports: [
     BrowserModule,
