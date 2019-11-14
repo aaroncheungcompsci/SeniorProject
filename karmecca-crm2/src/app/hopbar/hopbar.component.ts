7f1ab9bc02f601936fb1df66012d73811dcaeb45
Phu <phuhoangho@gmail.com>
2019-11-13 21:48:58 -0800

    added hopbar

---
+++ karmecca-crm2/src/app/hopbar/hopbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hopbar',
+  templateUrl: './hopbar.component.html',
+  styleUrls: ['./hopbar.component.css']
+})
+export class HopbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
