07c689d4da0c8992f32bbb09ebcc7c0fe10dae2a
Andrew Hang <andrewjameshang@csus.edu>
2019-12-01 14:59:57 -0800

    Andrew: Email button added

--- karmecca-crm2/src/app/app.module.ts
+++ karmecca-crm2/src/app/app.module.ts
@@ -20,6 +20,8 @@ import {FormsModule} from '@angular/forms';
 import { ViewAppsComponent } from './view-apps/view-apps.component';
 import { EmailButtonComponent } from './email-button/email-button.component';
 import { RegisterComponent } from './register/register.component';
+import { HttpService } from './Shared/http.service';
+
 
 
 /** TODO: remove when this is no longer needed */
@@ -131,9 +133,10 @@ import { HeaderComponent } from './header/header.component';
     MatSortModule,
     MatButtonModule,
     MatInputModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent],
   entryComponents: [ModalComponent]
 })
--- karmecca-crm2/src/app/email-button/email-button.component.ts
+++ karmecca-crm2/src/app/email-button/email-button.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from "@angular/core";
+import { HttpService } from "../Shared/http.service";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-email-button',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailButtonComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+
+  // ngOnInit() {
+  // }
+  loading = false;
+  buttionText = "Submit";
+
+  emailFormControl = new FormControl("", [
+    Validators.required,
+    Validators.email
+  ]);
+
+  constructor(public http: HttpService) {}
 
   ngOnInit() {
+    console.log(this.http.test);
   }
 
+  register() {
+    this.loading = true;
+    this.buttionText = "Submiting...";
+    let user = {
+      email: this.emailFormControl.value
+    }
+    this.http.sendEmail("http://localhost:3000/sendmail", user).subscribe(
+      data => {
+        let res:any = data; 
+        console.log(
+          `Reached register method`
+        );
+      },
+      err => {
+        console.log(err);
+        this.loading = false;
+        this.buttionText = "Submit";
+      },() => {
+        this.loading = false;
+        this.buttionText = "Submit";
+      }
+    );
+  }
 }
